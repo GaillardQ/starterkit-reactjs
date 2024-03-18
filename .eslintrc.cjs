a67f9b76aad72614d2d6535eb5115e43bca5a4ca
GaillardQ <quentin.gaillard.4@gmail.com>
2024-03-18 17:04:34 +0100

    Fix conf eslint/vite

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,100 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:react-hooks/recommended',
-	'plugin:@typescript-eslint/stylistic-type-checked',
-	'plugin:react/recommended',
-	'plugin:react/jsx-runtime'
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+const config = {
+    env: {
+        browser: true,
+        node: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    plugins: [
+        'react',
+        'react-hooks',
+        '@typescript-eslint'
     ],
-  },
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': [2, {
+            allowExpressions: true,
+            allowTypedFunctionExpressions: true,
+            allowHigherOrderFunctions: true,
+            allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+        }],
+        '@typescript-eslint/consistent-type-imports': [
+            'error',
+            {
+                prefer: 'type-imports',
+                disallowTypeAnnotations: false
+            }
+        ],
+        'arrow-parens': ['error', 'always'],
+        'arrow-body-style': ['error', 'as-needed'],
+        'eol-last': ['error', 'always'],
+        'import/no-unresolved': 'off',
+        'indent': ['error', 4],
+        'key-spacing':  ['error', {
+            singleLine: {
+                beforeColon: false,
+            },
+            multiLine: {
+                beforeColon: false,
+                afterColon: true,
+                mode: 'minimum'
+            }
+        }],
+        'max-len': [1, { code: 150 }],
+        'no-trailing-spaces': 'error',
+        'no-console': 1,
+        'no-multi-spaces': ['error', {
+            ignoreEOLComments: true,
+            exceptions: {
+                CallExpression: true,
+                ArrayExpression: true,
+                ObjectExpression: true,
+                ImportDeclaration: true,
+                VariableDeclarator: true,
+                TypeAliasDeclaration: true,
+                InterfaceDeclaration: true
+            }
+        }],
+        'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+        'no-var': 2,
+        'object-curly-spacing': [2, 'always'],
+        'quotes': [2, 'single'],
+        'quote-props': [2, 'consistent-as-needed'],
+        'react/display-name': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'off',
+        'react/jsx-curly-spacing': [
+            2,
+            {
+                when: 'always',
+                children: {
+                    when: 'always',
+                },
+                spacing: {
+                    objectLiterals: 'always',
+                },
+            },
+        ],
+        'react/prop-types': 0,
+        'semi': [2, 'always'],
+    }
+};
+
+module.exports = config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@app': path.resolve(__dirname, "./src/@app"),
-			'@core': path.resolve(__dirname, "./src/@core"),
-			'@ui': path.resolve(__dirname, "./src/@ui"),
-		},
-	},
-	server: {
-			port: 8080,
-		host: 'localhost',
-		open: true
-	},
-})
+    plugins: [
+        react()
+    ],
+    resolve: {
+        alias: {
+            '@app': path.resolve(__dirname, './src/@app'),
+            '@core': path.resolve(__dirname, './src/@core'),
+            '@ui': path.resolve(__dirname, './src/@ui'),
+        },
+    },
+    server: {
+        port: 8080,
+        host: 'localhost',
+        open: true
+    },
+});
